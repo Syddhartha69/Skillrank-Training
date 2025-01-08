8c816511650e3f0c7efe1195868e87eea288a4d5
Syddhartha Thapa <97379933+Syddhartha69@users.noreply.github.com>
2025-01-08 18:06:56 +0545

    Add files via upload

---
+++ Skillrank Training/Hello World.py
@@ -0,0 +1,4 @@
+# this is my first line of code in python 
+# this code is used to print "Hello World!" as output in the terminal
+
+print("Hello world!")  # this line of code is responsible for Printing output in the terminal 
