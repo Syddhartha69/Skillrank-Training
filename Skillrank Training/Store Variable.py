1936abee286a6f192bfe992494060f29c7e8beaf
Syddhartha Thapa <97379933+Syddhartha69@users.noreply.github.com>
2025-01-18 12:08:06 +0545

    Delete Skillrank Training/Store Variable.py

--- Skillrank Training/Store Variable.py
+++
@@ -1,6 +0,0 @@
-# this is my second code in python
-# the following code is used for storing value in a given variable 
-
-a = 5 # in this line of code, 'a' is the variable while '5' is the value to be stored 
-
-print(a) # this line of code is responsible for printing the output value stored in 'a'
